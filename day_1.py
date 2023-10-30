2a053e29b6469c5edb30b9ac66251b38ad56457f
JordanTK <kanchev.jordan@gmail.com>
2023-10-30 16:19:38 +0200

    day 1 completed

--- day_1.py
+++ day_1.py
@@ -1,1 +1,5 @@
+print("Hello folks, welcome to the band name generator!!!")
+city = input("What is your hometown?\n")
+pet = input("What is the name of your pet?\n")
+print(f"Your band name is {city} {pet}.")
 
