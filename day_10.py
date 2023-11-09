566c18124084208606c9e8993d5b2041194f39ed
JordanTK <kanchev.jordan@gmail.com>
2023-11-09 16:54:25 +0200

    day 10 completed

---
+++ day_10.py
@@ -0,0 +1,48 @@
+# Calculator
+
+# Clearing the console
+from os import system
+
+# Getting the logo
+from art_10 import logo
+
+# Add
+def add(n1,n2):
+  return n1+ n2
+
+# Substract
+def substract(n1,n2):
+  return n1-n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+# Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {"+": add, "-": substract, "*": multiply,  "/": divide}
+
+def calculator():
+  print(logo +"\n")
+  num1 = float(input("What's the first number?: "))
+  
+  should_continue = True
+  
+  while should_continue:
+    [print(operation) for operation in operations]
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = float(input("What's the second number?: "))
+    answer = operations[operation_symbol](num1,num2)
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      system("clear")
+      calculator()
+
+
+calculator()
